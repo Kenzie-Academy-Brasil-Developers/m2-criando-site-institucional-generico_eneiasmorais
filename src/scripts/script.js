70bdce9af224032f94c631bde9ae7748862888c1
Eneias Morais <eneiasmorais@gmail.com>
2023-01-24 15:36:28 -0300

    Entrega Realizada

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,27 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal(){
+    const button1 = document.querySelector('#subscribe__button')
+    const button2 = document.querySelector('#subscribe__button__2')
+    const container = document.querySelector('#modal__container')
+
+    button1.addEventListener('click', () => {
+        container.showModal()
+        closeModal()
+    })
+
+    button2.addEventListener('click', () => {
+        container.showModal()
+        closeModal()
+    })
+
+}
+handleModal()
+
+
+function closeModal(){
+    const closeButton = document.querySelector('#close__modal')
+    const container = document.querySelector('#modal__container')
+
+    closeButton.addEventListener('click', () => {
+        container.close()
+    })
+}
